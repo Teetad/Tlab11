6c93367765717e64ecf7aa54209bb4e006df7cd0
Teetad <teetadraseesit@gmail.com>
2024-12-24 12:38:37 +0700

    lab11_1 done

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,27 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+
+using namespace std;
+
+int main(){
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    srand(time(0));
+    string grade;
+    int random_num = rand() % 9;
+    if (random_num == 0) grade = "A";
+    if (random_num == 1) grade = "B+";
+    if (random_num == 2) grade = "B";
+    if (random_num == 3) grade = "C+";
+    if (random_num == 4) grade = "C";
+    if (random_num == 5) grade = "D+";
+    if (random_num == 6) grade = "D";
+    if (random_num == 7) grade = "F";
+    if (random_num == 8) grade = "W";
+    cout << "You will get " << grade <<" in this 261102.";
+
+
+}
